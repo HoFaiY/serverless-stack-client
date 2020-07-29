ba28d16bfd4b8156c9faec33c8cede4f3ae86058
unknown <hofaiyau@gmail.com>
2020-07-30 02:22:02 +0800

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
 export default {
+    STRIPE_KEY: "pk_test_51H34feINFswwa8d1naCqMXypXmTnKtm6dZyBPTsef8nQoDu5yp4BPoneeOO05FUjt5Q0veDIsEocnvqJKQqUy3Ml005BUBKI9U",
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "us-east-1",
       BUCKET: "notes-app-upload-yhf"
     },
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://4aa0f0bwp8.execute-api.us-east-1.amazonaws.com/prod/"
+      URL: "https://4aa0f0bwp8.execute-api.us-east-1.amazonaws.com/prod"
     },
     cognito: {
       REGION: "us-east-1",
